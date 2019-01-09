cc783457665c9863245108748b26a590e274204b
Jorge Eremiev <jorgeche@Jorges-MacBook-Pro-2.local>
2019-01-09 15:23:36 -0500

    Cleaned up first empty char produced by grit.

--- assets/images/MenuCursor/Binary/MenuCursor.c
+++ assets/images/MenuCursor/Binary/MenuCursor.c
@@ -4,7 +4,7 @@
 //======================================================================
 //
 //	MenuCursor, 8x24@2, 
-//	+ 4 tiles not compressed
+//	+ 3 tiles not compressed
 //	+ regular map (flat), not compressed, 1x3 
 //	Total size: 64 + 8 = 72
 //
@@ -15,13 +15,13 @@
 
 const unsigned int MenuCursorTiles[16] __attribute__((aligned(4)))=
 {
-	0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00540014,0x00540154,0x00000014,
+	0x00000000,0x00540014,0x00540154,0x00000014,
 	0x00000000,0x01500050,0x01500550,0x00000050,0x00000000,0x05400140,0x05401540,0x00000140,
 };
 
 const unsigned short MenuCursorMap[4] __attribute__((aligned(4)))=
 {
-	0x0001,0x0002,0x0003,0x0000,
+	0x0000
 };
 
 //}}BLOCK(MenuCursor)
